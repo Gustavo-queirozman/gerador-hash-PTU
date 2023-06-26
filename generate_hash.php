0593ae170a9885f4877e6d5d045a67f92cbaf8b7
Gustavo-queirozman <gustavoqueirozmit@gmail.com>
2023-06-26 08:02:47 -0300

    first commit

---
+++ generate_hash.php
@@ -0,0 +1,16 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['arquivo'])) {
+  $file_path = $_FILES['arquivo']['tmp_name'];
+
+$extension = pathinfo($file_path, PATHINFO_EXTENSION);
+// Insira o caminho completo para o arquivo aqui
+echo $extension;
+$file_content = file_get_contents($file_path); // Leitura do arquivo
+$file_content = str_replace(array("\r", "\n"), '', $file_content); // Remoção de quebras de linha
+
+$hash = md5(mb_convert_encoding(substr($file_content, 0, -43), 'ISO-8859-1')); // Criação do hash utilizando a codificação ISO-8859-1
+
+
+echo "Seu novo hash é: $hash\nAltere o registro 998 (Posição 12 a 43) e valide o arquivo.\n\n";
+fgets(STDIN); // Espera a entrada do usuário para sair
+}
